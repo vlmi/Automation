51e30b2204a7eb16291ce6a135ac8daa1d9446ab
Vlad M <deserthetman@gmail.com>
2015-02-16 19:02:05 +0200

    Russian fixed

--- Full flow.rb
+++ Full flow.rb
@@ -3,15 +3,26 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 driver.get 'http://demo.redmine.org'
 
-#login variable = testuser+(random)
-#email variable = vlmiruby+(random)@gmail.com
+#login1 variable = testuser1+(random)
+#login2 variable = testuser1+1+(random)
+#email1 variable = vlmiruby+(random)@gmail.com
+#email2 variable = vlmiruby+1+(random)@gmail.com
 #pass variable = qwerty+(random)
 #new pass variable = pass+1
 #project_name variable = ProjectName+(random)
 
+driver.find_element(class: 'register').click #register new user1
+driver.find_element(id: 'user_login').send_keys 'testuser13'
+driver.find_element(id: 'user_password').send_keys 'qwerty'
+driver.find_element(id: 'user_password_confirmation').send_keys 'qwerty'
+driver.find_element(id: 'user_firstname').send_keys 'Firstname'
+driver.find_element(id: 'user_lastname').send_keys 'Lastname'
+driver.find_element(id: 'user_mail').send_keys 'vlmiruby+13@gmail.com'
+driver.find_element(name: 'commit').click
 
+driver.find_element(class: 'logout').click #logout
 
-driver.find_element(class: 'register').click #register new user
+driver.find_element(class: 'register').click #register new user2
 driver.find_element(id: 'user_login').send_keys 'testuser13'
 driver.find_element(id: 'user_password').send_keys 'qwerty'
 driver.find_element(id: 'user_password_confirmation').send_keys 'qwerty'
@@ -28,14 +39,14 @@ driver.find_element(id: 'password').send_keys 'qwerty'
 driver.find_element(name: 'login').click
 
 driver.find_element(class: 'my-account').click #change password
-driver.find_element(link: 'Изменить пароль').click
+driver.find_element(class: 'icon-passwd').click
 driver.find_element(id: 'password').send_keys 'qwerty'
 driver.find_element(id: 'new_password').send_keys 'qwerty1'
 driver.find_element(id: 'new_password_confirmation').send_keys 'qwerty1'
 driver.find_element(name: 'commit').click
 
 driver.find_element(class: 'projects').click #create new project
-driver.find_element(link: 'Новый проект').click
+driver.find_element(class: 'icon-add').click
 driver.find_element(:id, "project_name").clear
 driver.find_element(id: 'project_name').send_keys 'TestProject13'
 driver.find_element(name: 'commit').click
@@ -46,9 +57,9 @@ driver.find_element(name: 'membership[user_ids][]').click
 driver.find_element(:xpath, "(//input[@name='membership[role_ids][]'])[7]").click
 driver.find_element(id: 'member-add-submit').click
 
-#driver.find_element(:css, "#member-90327 > td.buttons > a.icon.icon-edit").click #change users role
-#driver.find_element(:xpath, "(//input[@name='membership[role_ids][]'])[6]").click
-#driver.find_element(:xpath, "(//input[@name='commit'])[4]").click
+driver.find_element(:css, "#member-90327 > td.buttons > a.icon.icon-edit").click #change users role
+driver.find_element(:xpath, "(//input[@name='membership[role_ids][]'])[6]").click
+driver.find_element(:xpath, "(//input[@name='commit'])[4]").click
 
 driver.find_element(id: 'tab-versions').click #add version
 driver.find_element(link: 'Новая версия').click
