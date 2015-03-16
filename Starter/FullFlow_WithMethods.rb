efc7dc5d09bf41961f95e1ac9331c3fb99b40f7e
Vlad M <deserthetman@gmail.com>
2015-03-16 15:59:04 +0200

    fixes to main successful flow

--- Starter/FullFlow_WithMethods.rb
+++ Starter/FullFlow_WithMethods.rb
@@ -21,9 +21,9 @@ number = rand(100..2000)
 @manager = "(//input[@name='membership[role_ids][]'])[5]"
 @developer = "(//input[@name='membership[role_ids][]'])[6]"
 @reporter = "(//input[@name='membership[role_ids][]'])[7]"
-@bug = "//*[@id='issue_tracker_id']/option[1]"
-@feature = "//*[@id='issue_tracker_id']/option[2]"
-@support = "//*[@id='issue_tracker_id']/option[3]"
+@bug = 'Bug'
+@feature = 'Feature'
+@support = 'Support'
 
 def register_new_user(user_login, email)
   @driver.find_element(class: 'register').click
@@ -35,62 +35,77 @@ def register_new_user(user_login, email)
   @driver.find_element(id: 'user_mail').send_keys email
   @driver.find_element(name: 'commit').click
 end
+
 def log_out
   @driver.find_element(class: 'logout').click
 end
+
 def log_in(login, pass)
   @driver.find_element(class: 'login').click
   @driver.find_element(id: 'username').send_keys login
   @driver.find_element(id: 'password').send_keys pass
- @driver.find_element(name: 'login').click
+  @driver.find_element(name: 'login').click
 end
+
 def change_password(oldpass, newpass)
-@driver.find_element(class: 'my-account').click
-@driver.find_element(:css, '.icon-passwd').click
-@driver.find_element(id: 'password').send_keys oldpass
-@driver.find_element(id: 'new_password').send_keys newpass
-@driver.find_element(id: 'new_password_confirmation').send_keys newpass
-@driver.find_element(name: 'commit').click
+  @driver.find_element(class: 'my-account').click
+  @driver.find_element(:css, '.icon-passwd').click
+  @driver.find_element(id: 'password').send_keys oldpass
+  @driver.find_element(id: 'new_password').send_keys newpass
+  @driver.find_element(id: 'new_password_confirmation').send_keys newpass
+  @driver.find_element(name: 'commit').click
 end
+
 def create_new_project(project)
-@driver.find_element(class: 'projects').click
-@driver.find_element(:css, '.icon-add').click
-@driver.find_element(:id, "project_name").clear
-@driver.find_element(id: 'project_name').send_keys project
-@driver.find_element(name: 'commit').click
+  @driver.find_element(class: 'projects').click
+  @driver.find_element(:css, '.icon-add').click
+  @driver.find_element(:id, "project_name").clear
+  @driver.find_element(id: 'project_name').send_keys project
+  @driver.find_element(name: 'commit').click
 end
+
 def add_user(user, role)
   @driver.find_element(id: 'tab-members').click
-  @driver.find_element(id: 'principal_search').send_keys user
+  @driver.find_element(:xpath, "//div[@id='tab-content-members']/p/a").click
+  sleep 2
+  @driver.find_element(:xpath, ".//*[@id='principal_search']").send_keys user
   sleep 2
   @driver.find_element(name: 'membership[user_ids][]').click
   @driver.find_element(:xpath, "#{role}").click
   @driver.find_element(id: 'member-add-submit').click
   sleep 3
 end
+
 def change_role(role)
   @driver.find_element(:css, '.even a.icon-edit').click
   sleep 3
   @driver.find_element(:xpath, "#{role}").click
   @driver.find_element(:xpath, "(//input[@name='commit'])[4]").click
 end
+
 def add_version(version)
   @driver.find_element(id: 'tab-versions').click
-  @driver.find_element(:css, '.icon-add').click
+  @driver.find_element(:xpath, "//*[@id='tab-content-versions']/p[2]/a").click
   @driver.find_element(id: 'version_name').send_keys version
   @driver.find_element(name: 'commit').click
 end
+
 def create_task(type, name)
   @driver.find_element(class: 'new-issue').click
-  @driver.find_element(id: "issue_tracker_id").click
-  @driver.find_element(:xpath, type).click
+  sleep 2
+  drop_down_menu = @driver.find_element(id: 'issue_tracker_id')
+  option = Selenium::WebDriver::Support::Select.new(drop_down_menu)
+  option.select_by(:text, type)
+  sleep 2
   @driver.find_element(id: 'issue_subject').send_keys name
   @driver.find_element(name: 'commit').click
 end
+
 def show_tasks
   @driver.find_element(:css, '.issues.selected').click
   sleep 2
 end
+
 def check_visibility(link_name)
   if @driver.find_element(:link, link_name).displayed?
     puts "#{link_name} present on page"
