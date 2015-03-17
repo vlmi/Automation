64c12f985bfa9751dc76a970a411e5e7ae220923
Vlad M <deserthetman@gmail.com>
2015-03-17 18:02:27 +0200

    finally something started to work

--- RSpecRedmine/spec/fullflow_spec.rb
+++ RSpecRedmine/spec/fullflow_spec.rb
@@ -3,27 +3,18 @@ require_relative '../pages/home_page'
 require_relative '../pages/register_page'
 
 describe "Full redmine site test flow" do
-  before :all do
+    context "When registering new user" do
+    before :all do
+      visit(RegisterPage)
+      end
+    it "new user should be successfully registered" do
+    on(RegisterPage).login = 'Testuser'
+    on(RegisterPage).password = 'qwerty'
+    on(RegisterPage).password_confirm = 'qwerty'
+    on(RegisterPage).first_name = 'lol'
+    on(RegisterPage).last_name = 'lolita'
+    on(RegisterPage).email = 'Testuser@gmail.com'
+    on(RegisterPage).commit
   end
-  context "When registering new user" do
-    before do
-      visit(RegisterPage).register_new_user(fggfd)
-      on(HomePage).wait_until{@current_page.text.include}
     end
-    it "new user should be successfully registered"
-
-  end
-  it_behaves_like
-  context "When Logging in" do
-    it "user should log in"
-
-  end
-  describe "Log out" do
-  end
-  describe "Change password" do
-  end
-  describe "Create new project" do
-  end
-  describe "Add new user to project" do
-  end
 end
